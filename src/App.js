b9bb36e4ec68ea95186180b309b446d08d2f94ee
Candra Fried <candracodes@gmail.com>
2021-12-30 13:42:02 -0600

    Successful implementation of Nav component.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import Hero from './components/Hero';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* MAIN CONTAINER (start) ================================================= */}
+      <div className="ts-page-wrapper" id="page-top">
+      <Nav />
+      </div>
+      {/* MAIN CONTAINER (end) ================================================= */}
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import logo from './assets/img/logo.png'
+
+export default function Nav() {
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light fixed-top bg-white">
+            <div className="container">
+                <a className="navbar-brand ts-push-down__50 position-absolute ts-bottom__0 bg-white pb-0 ts-z-index__1 ts-scroll"
+                    href="#page-top">
+                    <img src={logo} alt="White text against Pink background of Candra Fried logo." />
+                </a>
+                {/* <!--end navbar-brand--> */}
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                {/* <!--end navbar-toggler--> */}
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav d-block d-lg-flex ml-auto text-right">
+                        <a className="nav-item nav-link active ts-scroll" href="#page-top">Home <span
+                                className="sr-only">(current)</span></a>
+                        <a className="nav-item nav-link ts-scroll" href="#about-me">About Me</a>
+                        <a className="nav-item nav-link ts-scroll" href="#portfolio">Portfolio</a>
+                        <a className="nav-item nav-link ts-scroll" href="#my-skills">Skills</a>
+                        <a className="nav-item nav-link ts-scroll" href="#contact">Contact</a>
+                        <a className="nav-item nav-link ts-scroll mr-2"
+                            href="https://www.etsy.com/shop/FriedCookieCreations" target="_blank" rel="noreferrer">Etsy</a>
+                    </div>
+                    {/* <!--end navbar-nav--> */}
+                </div>
+                {/* <!--end collapse--> */}
+            </div>
+            {/* <!--end container--> */}
+        </nav>
+    
+  );
+}
